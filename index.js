6855a6bf0b03ba16869aef64aaed87496e13ee5a
XPasy <145772936+XPasy@users.noreply.github.com>
2023-09-22 19:11:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const btns = document.querySelector('.btns');
 const btn = document.querySelectorAll('.btn');
+const inputChange = document.querySelector('.change-input');
+const TextChange = document.getElementById('change-text');
+const spreadText = document.getElementById('spread-text');
 
-let money = 100.01;
+let money = 1000.01;
+let spread = 5;
 
-document.getElementById('money').innerHTML = money + '$'
-
-btns.addEventListener("click", function(event){
-    currentElement = event.target;
-    currentElement.classList.add('active');
+window.addEventListener('load', () => {
+    spread = localStorage.getItem('spread');
+    TextChange.innerText = 'Спред: ' + spread + "%";
+    spreadText.innerText = 'Текущий спред: ' + spread + "%";
 });
+
+document.getElementById('money').innerHTML = money + '$';
+
+function show_change(){
+    document.querySelector('.change').classList.toggle('none');
+}
+
+function save_spread(){
+    spread = inputChange.value;
+    localStorage.setItem('spread', spread);
+    if(inputChange.value != ''){
+        TextChange.innerText = 'Текущий спред: ' + spread + "%";
+    }
+}
