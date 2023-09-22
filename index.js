69669d21fc8f327087956055869f2a371d21a84f
XPasy <145772936+XPasy@users.noreply.github.com>
2023-09-22 16:47:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const btns = document.querySelector('.btns');
 const btn = document.querySelectorAll('.btn');
 
+let money = 100.01;
+
+document.getElementById('money').innerHTML = money + '$'
+
 btns.addEventListener("click", function(event){
     currentElement = event.target;
     currentElement.classList.add('active');
